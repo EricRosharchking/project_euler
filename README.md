ec23f5ebe49ffb73f22e39920b539bacb0266eda
Liyuan <liluoke9@gmail.com>
2020-12-22 17:49:27 +0800

    Adding WIP file, 1 allowed at any time.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a (likely going to fail) attempt for the project Euler challenge
 
 Commit Message format: 
     Problem X solution
+
+Problem.java:
+    The problem that I am working on
---
+++ problem.java
@@ -0,0 +1,9 @@
+public class problem {
+    public static void main(String[] args) {
+        // find the least common multiple of integers from 1 to 20;
+        int intermediate = 2;
+        for (int i = 2; i <= 20; i++) {
+            
+        }
+    }
+}
