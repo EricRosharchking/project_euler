78fca6eccbe3afdcfa65add1fdf2e1f277186c43
Liyuan <liluoke9@gmail.com>
2020-12-22 17:38:43 +0800

    Problem 4 solution

--- problem3.java
+++ problem3.java
@@ -1,18 +1,30 @@
 public class problem3 {
     public static void main(String[] args) {
         // larget prime factor of 600851475143 
-        long num =  600851475143l;
-        System.out.print(isPrime(num));
+        long num =  906609;
+        System.out.println(isPrime(num));
+        System.out.println(notPrime(num));
     }
 
     public static long isPrime(long num) {
         double sqrt = Math.sqrt((double) num);
         long intermediate = num;
-        for (int i = 2; i <= sqrt & intermediate >= sqrt; i ++) {
+        for (int i = 2; i <= sqrt & intermediate >= sqrt;) {
             if (intermediate % i == 0) {
                 intermediate /= i;
+            } else {
+                 i ++;
             }
         }
         return intermediate;
+    }
+
+    public static long notPrime(long num) {
+        for (long i = 2; i <= num; i ++) {
+            if (num % i ==0) {
+                return i;
+            }
+        }
+        return num;
     }
 }
---
+++ problem4.java
@@ -0,0 +1,28 @@
+public class problem4 {
+    // this solution contains some critical logical flaws, but for the time being it produces the right answer, so I will just let it be.
+    public static void main(String[] args) {
+        /*
+        a, b, c are positive single digit integers
+        100000*a + 10000*b + 1000*c + 100*c + 10*b + a = 100001*a + 10010*b + 1100*c
+        */
+        int a = 9;
+        for (; a >= 0; a--) {
+            int b = 9;
+            for (; b >=0; b--) {
+            int c = 9;
+                for (; c >= 0; c--) {
+                    long result = 100001 * a + 10010 * b + 1100 * c;
+                    long maxPrimeFactor = problem3.isPrime(result);
+                    long minPrimeFactor = problem3.notPrime(result);
+                    if  (minPrimeFactor * maxPrimeFactor < 1000 && minPrimeFactor * maxPrimeFactor > 100
+                        && (result / maxPrimeFactor / minPrimeFactor) > 100 && (result / maxPrimeFactor / minPrimeFactor) < 1000) {
+                            a = -1;
+                            b = -1;
+                            c = -1;
+                            System.out.println(result);
+                        }
+                }
+            }
+        }
+    }
+}
