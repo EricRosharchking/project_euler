7fd206a54dd243247f669317932f4b928af26590
EricRosharchking <liluoke9@gmail.com>
2020-12-23 15:51:36 +0800

    Updated problem3 solution after EulerUtil.java is added.

--- problem3.java
+++ problem3.java
@@ -1,22 +1,21 @@
 public class problem3 {
     public static void main(String[] args) {
         // larget prime factor of 600851475143 
-        long num =  906609;
-        System.out.println(isPrime(num));
-        System.out.println(notPrime(num));
+        long num = 600851475143l;
+        System.out.println(EulerUtil.getMaxPrimeFactor(num));
     }
 
     public static long isPrime(long num) {
         double sqrt = Math.sqrt((double) num);
-        long intermediate = num;
-        for (int i = 2; i <= sqrt & intermediate >= sqrt;) {
-            if (intermediate % i == 0) {
-                intermediate /= i;
+        long result = num;
+        for (int i = 2; i < result && i <= sqrt;) {
+            if (result % i == 0) {
+                result /= i;
             } else {
                  i ++;
             }
         }
-        return intermediate;
+        return result;
     }
 
     public static long notPrime(long num) {
