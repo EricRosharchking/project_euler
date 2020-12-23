238ca3b8c0d7d60c002bc3c45905a4bf543182c7
EricRosharchking <liluoke9@gmail.com>
2020-12-23 18:00:59 +0800

    Problem 7 solution.

--- problem7.java
+++ problem7.java
@@ -1,10 +1,10 @@
 public class problem7 {
-    
+
     public static void main(String[] args) {
         // 10001 st prime number
         int count = 2;
         int i = 3;
-        for (; count < 10001; i += 2) {
+        for (; count <= 10002; i += 2) {
             if (EulerUtil.isPrime(i))
                 count ++;
         }
