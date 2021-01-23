560cf8fe2228453e41ff77c8bc3d5dbb5a1fa388
Liyuan <liluoke9@gmail.com>
2021-01-23 19:35:14 +0800

    Adopted the Runnable/ThreadPool pattern for problem 14.

--- problem14.java
+++ problem14.java
@@ -1,26 +1,50 @@
+import java.security.interfaces.EdECKey;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class problem14 {
     public static void main(String[] args) {
-        Collatz collatz = new Collatz();
-        int result = 0;
-        long element = 999999L;
-        for (long num = 999999L; num >= 500000; num--) {
-            int size = collatz.getCollatzSequence(num).size();
-            if (result < size) {
-                result = size;
-                element = num;
+        // Collatz collatz = new Collatz();
+        // int result = 0;
+        // long element = 999999L;
+        // for (long num = 999999L; num >= 500000; num--) {
+        //     int size = collatz.getCollatzSequence(num).size();
+        //     if (result < size) {
+        //         result = size;
+        //         element = num;
+        //     }
+        // }
+        // System.out.println(element);
+        // System.out.println(result);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        try {
+            for (long num = 999999L; num >= 549999; num -= 50000) {
+                executorService.submit(new Collatz(num));
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println(element);
-        System.out.println(result);
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+    }
+
+    public static void calculate(Long element) {
+
     }
 }
 
-class Collatz {
+class Collatz implements Runnable{
 
-    public Collatz() {}
+    private Long firstLong;
+    private Long lastLong;
+
+    public Collatz(Long firstLong) {
+        this.firstLong = firstLong;
+        lastLong = firstLong - 50000;
+    }
     
     public List<Long> getCollatzSequence(Long element) {
         List<Long> sequence =  new ArrayList<>();
@@ -37,5 +61,18 @@ class Collatz {
             return (element * 3 + 1);
         }
         return element /= 2;
+    }
+
+    public void run() {
+        int result = 0;
+        long element = firstLong;
+        for (long num = firstLong; num >= lastLong; num--) {
+            int size = getCollatzSequence(num).size();
+            if (result < size) {
+                result = size;
+                element = num;
+            }
+        }
+        System.out.println(result + ", " + element);
     }
 }
